b031abcf467419563a096886eea01d04f50ec681
Emil Zegers <e.zegers@upcmail.nl>
2021-03-24 09:16:46 +0100

    Update query_grid.py
    updates

--- query_grid.py
+++ query_grid.py
@@ -6,6 +6,7 @@ from pymongo import MongoClient
 
 # TODO: replace YOUR_CONNECTION_STRING_HERE with your own Atlas connection string for Python in the next line, and leave out the Pymongo prefix
 client = YOUR_CONNECTION_STRING_HERE
+db = client.test
 
 filter={
     'geometry': {
@@ -19,8 +20,8 @@ filter={
     }
 }
 
-result = client['test']['gridder'].find(
-  filter=filter
+result = db['gridder'].find(
+    filter=filter
 )
 
 for document in result: 
