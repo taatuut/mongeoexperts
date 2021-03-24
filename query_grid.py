e4067cfcef2faa371804c983fd810d40ae267a44
Emil Zegers <e.zegers@upcmail.nl>
2021-03-24 08:30:49 +0100

    Update query_grid.py
    Code cleanup

--- query_grid.py
+++ query_grid.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 # Examples:
 #client = MongoClient('mongodb://localhost:27017/test')
-#client = MongoClient("mongodb+srv://<user>:<pass>@yourserver.at.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+#client = MongoClient("mongodb+srv://<user>:<pass>@yourserver.at.mongodb.net/test?retryWrites=true&w=majority")
 
 # TODO: replace YOUR_CONNECTION_STRING_HERE with your own Atlas connection string for Python in the next line, and leave out the Pymongo prefix
 client = YOUR_CONNECTION_STRING_HERE
