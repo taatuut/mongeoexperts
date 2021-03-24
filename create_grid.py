8cac74b615b2777915c2ad1e71ac4e82374a7373
Emil Zegers <e.zegers@upcmail.nl>
2021-03-24 08:25:51 +0100

    Update create_grid.py
    Code cleanup

--- create_grid.py
+++ create_grid.py
@@ -1,36 +1,33 @@
-import decimal
-
 def gridder():
-    pos = 1
-    lonmin = -180
-    lonmax = 180
-    latmin = -90
-    latmax = 90
+# function gridder creates grid point data in geojson format
+    pointid = 1
+    lonmin, lonmax, latmin, latmax = -180, 180, -90, 90
+    orglatmin = latmin
     stepsize = 10
     while lonmin < lonmax:
         gj = {
-        "type": "Feature",
-        "geometry": {
-            "type": "Point",
-            "coordinates": [float(lonmin), float(latmin)]
-        },
-        "properties": {
-            "name": "Point" + str(pos)
-        }
+          "type": "Feature",
+          "geometry": {
+              "type": "Point",
+              "coordinates": [float(lonmin), float(latmin)]
+          },
+          "properties": {
+              "name": "Point" + str(pointid)
+          }
         }
         yield(gj)
-        latmin += decimal.Decimal(stepsize)
-        pos += 1
-        if latmin == 90:
-            latmin = -90
-            lonmin += decimal.Decimal(stepsize)
+        latmin += stepsize
+        pointid += 1
+        if latmin == latmax:
+            latmin = orglatmin
+            lonmin += stepsize
 
-
+# print the output in a json array
 i = 0
 print("[")
 for g in gridder():
     if i > 0:
       print(",")
-    print(str(g).replace("'","\""))
+    print(str(g))
     i += 1
 print("]")
